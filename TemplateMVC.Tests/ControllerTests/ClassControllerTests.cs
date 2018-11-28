6417ccc1092f83068045e19e4114934fc0eed2dd
Student <example@example.com>
2018-11-28 12:39:27 -0800

    add class controller tests file.

---
+++ TemplateMVC.Tests/ControllerTests/ClassControllerTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TemplateMVC.Controllers;
+using TemplateMVC.Models;
+
+namespace TemplateMVC.Tests
+{
+  [TestClass]
+  // RENAME THIS CLASS TO MATCH THE CLASS YOU ARE WORKING WITH
+  public class CLASSControllerTest
+  {
+    
+  }
+}
