a4f69a188937461159ec402884fe6bc559b96475
Student <example@example.com>
2018-12-04 15:38:50 -0800

    refactor basic files so that home controller test fails and the app will run on localhost with an empty page.

--- TemplateMVC.Tests/ControllerTests/BasicClassControllerTests.cs
+++ TemplateMVC.Tests/ControllerTests/BasicClassControllerTests.cs
@@ -6,10 +6,9 @@ using TemplateMVC.Models;
 
 namespace TemplateMVC.Tests
 {
-  // [TestClass]
-  // // RENAME THIS CLASS TO MATCH THE CLASS YOU ARE WORKING WITH
-  // public class CLASSControllerTest
-  // {
-  //
-  // }
+  [TestClass]
+  public class BasicClassControllerTest
+  {
+
+  }
 }
---
+++ TemplateMVC.Tests/ModelTests/BasicClassTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using TemplateMVC.Models;
+
+namespace TemplateMVC.Tests
+{
+  [TestClass]
+  public class BasicClassTests
+  {
+
+  }
+}
