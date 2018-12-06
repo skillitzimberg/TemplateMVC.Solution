b568f39d1da450c514cff45194f5bdbe0aa37815
Student <example@example.com>
2018-12-06 07:10:42 -0800

    remove class controller and controllertest files and some unnecessary using directives.

--- TemplateMVC.Tests/ControllerTests/HomeControllerTests.cs
+++ TemplateMVC.Tests/ControllerTests/HomeControllerTests.cs
@@ -1,8 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using TemplateMVC.Controllers;
-using TemplateMVC.Models;
 
 namespace TemplateMVC.Tests
 {
--- TemplateMVC/Controllers/HomeController.cs
+++ TemplateMVC/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using TemplateMVC.Models;
 
 namespace TemplateMVC.Controllers
 {
